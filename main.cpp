4aaa20c8faa56f37dbf66260f7d133a0b01858c0
emiliogonzalez-max <emilio.gonzalez@lasallesaltillo.edu.mx>
2026-09-22 23:01:30 -0600

    Add loop to sum user-input numbers
    Implemented a loop to read numbers and calculate their sum.

--- main.cpp
+++ main.cpp
@@ -1,24 +1,21 @@
-// ¿Recuerdas qué hace iostream?
 #include <iostream>
 
-// ¿por qué debe existir la función main()?
 int main() {
-    // 1. Constante: valor que no cambia durante la ejecución
+
     const int CANTIDAD = 5;
 
-    // 2. Variables (siempre inicializadas)
     double suma   = 0.0;
     double numero = 0.0;
 
     std::cout << "Suma de " << CANTIDAD << " numeros\n";
 
-    // 3. Ciclo: repetir CANTIDAD veces
-    //    TODO: elige un ciclo (for o while) y usa un contador
-    //    TODO: pedir el numero, leerlo y acumularlo en suma
+    for (int i = 1; i <= CANTIDAD; i++) {
+        std::cout << "Ingresa el numero " << i << ": ";
+        std::cin >> numero;
+        suma = suma + numero;
+    }
 
-    // 4. Salida
-    //    TODO: mostrar el total
+    std::cout << "La suma total es: " << suma << "\n";
 
-    // ¿Qué significa return 0;?
     return 0;
 }
